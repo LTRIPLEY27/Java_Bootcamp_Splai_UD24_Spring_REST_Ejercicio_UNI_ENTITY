f7d9456bcc92152e563d0b93102a35132b2af082
IsaDevs <isabel.calzadilla.18@gmail.com>
2022-02-22 11:22:30 +0100

    intento de applicacion spring con api

---
+++ trabajador/src/main/java/CrudAplicationH2.java
@@ -0,0 +1,10 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudAplicationH2 {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(CrudAplicationH2.class, args); //HACE EL LLAMADO A LA CLASE PARA QUE ARRANQUE
+	}
+}
